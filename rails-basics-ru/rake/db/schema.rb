d04068b8e417b9eabfaaeb2234d7b10dc5076229
@hexlet/cli <support@hexlet.io>
2025-03-17 23:38:27 +0300

    submit rails-basics-ru/rake

--- rails-basics-ru/rake/db/schema.rb
+++ rails-basics-ru/rake/db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2022_09_16_161135) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_17_190240) do
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_users", force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_courses_users_on_course_id"
+    t.index ["user_id"], name: "index_courses_users_on_user_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "state"
+    t.string "title"
+    t.text "body"
+    t.integer "creator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_posts_on_creator_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,5 +53,5 @@ ActiveRecord::Schema[7.2].define(version: 2022_09_16_161135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
 end
--- rails-basics-ru/rake/test/tasks/import_users_test.rb
+++ rails-basics-ru/rake/test/tasks/import_users_test.rb
@@ -2,6 +2,7 @@
 
 require 'test_helper'
 require 'rake'
+require 'csv'
 
 class ImportUsersTest < ActiveSupport::TestCase
   def setup
@@ -13,8 +14,22 @@ class ImportUsersTest < ActiveSupport::TestCase
     path = File.join test_dir_path, 'fixtures/files/users.csv'
 
     # FIXME: use find_by instead of assert difference
-    assert_difference('User.count', +100) do
+    test 'import users' do
+      test_dir_path = File.dirname(__FILE__, 2)
+      path = File.join test_dir_path, 'fixtures/files/users.csv'
+  
+      imported_emails = []
+  
+      CSV.foreach(path, headers: true) do |row|
+        imported_emails << row['email']
+      end
+  
       Rake::Task['hexlet:import_users'].invoke(path)
+  
+      imported_emails.each do |email|
+        user = User.find_by(email:)
+        assert_not_nil user, "User with email #{email} was not imported"
+      end
     end
   end
 end
