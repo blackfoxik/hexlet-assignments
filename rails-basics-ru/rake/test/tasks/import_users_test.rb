4541212562ba01a797eba1010943da3db1904ea1
@hexlet/cli <support@hexlet.io>
2025-03-17 23:43:32 +0300

    submit rails-basics-ru/rake

--- rails-basics-ru/rake/test/tasks/import_users_test.rb
+++ rails-basics-ru/rake/test/tasks/import_users_test.rb
@@ -13,23 +13,17 @@ class ImportUsersTest < ActiveSupport::TestCase
     test_dir_path = File.dirname(__FILE__, 2)
     path = File.join test_dir_path, 'fixtures/files/users.csv'
 
-    # FIXME: use find_by instead of assert difference
-    test 'import users' do
-      test_dir_path = File.dirname(__FILE__, 2)
-      path = File.join test_dir_path, 'fixtures/files/users.csv'
+    imported_emails = []
 
-      imported_emails = []
+    CSV.foreach(path, headers: true) do |row|
+      imported_emails << row['email']
+    end
 
-      CSV.foreach(path, headers: true) do |row|
-        imported_emails << row['email']
-      end
+    Rake::Task['hexlet:import_users'].invoke(path)
 
-      Rake::Task['hexlet:import_users'].invoke(path)
-
-      imported_emails.each do |email|
-        user = User.find_by(email:)
-        assert_not_nil user, "User with email #{email} was not imported"
-      end
+    imported_emails.each do |email|
+      user = User.find_by(email:)
+      assert_not_nil user, "User with email #{email} was not imported"
     end
   end
 end
