3eeac73bf2820e2167ce6e44c8ff2a057cd7db55
@hexlet/cli <support@hexlet.io>
2025-03-17 23:40:16 +0300

    submit rails-basics-ru/rake

--- rails-basics-ru/rake/test/tasks/import_users_test.rb
+++ rails-basics-ru/rake/test/tasks/import_users_test.rb
@@ -17,15 +17,15 @@ class ImportUsersTest < ActiveSupport::TestCase
     test 'import users' do
       test_dir_path = File.dirname(__FILE__, 2)
       path = File.join test_dir_path, 'fixtures/files/users.csv'
-  
+
       imported_emails = []
-  
+
       CSV.foreach(path, headers: true) do |row|
         imported_emails << row['email']
       end
-  
+
       Rake::Task['hexlet:import_users'].invoke(path)
-  
+
       imported_emails.each do |email|
         user = User.find_by(email:)
         assert_not_nil user, "User with email #{email} was not imported"
